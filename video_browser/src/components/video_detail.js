68d718135610abc26fa449e4ab55c3e404a59ca7
RahulMisra2000 <RahulMisra2000@users.noreply.github.com>
2018-05-20 13:15:54 -0400

    Comments

--- video_browser/src/components/video_detail.js
+++ video_browser/src/components/video_detail.js
@@ -1,6 +1,18 @@
 import React from "react";
 
+// let/const {age} = {name:’john’, age:45};
+// What this does is, it creates a variable or constant  called age that will contain the value of the age property so, 
+// it will contain 45
+// Use Case: This is especially useful as formal parameters in function declaration because we can pick and choose a 
+// part of the larger actual parameter object of the function invocation.
+// In the example below 'props' is shoved by the system as the parameter to the arrow function.... but we are 
+// only interested in props.video   ...hence ... that is why we are doing { video }
+
+
+// This is a function based COMPONENT. And function based components return what needs to be rendered in their return value
+// Class based components have a render() method that they define and that is what is rendered when the component is rendered
 const VideoDetail = ({ video }) => {
+   
   if (!video) {
     return <div>Loading...</div>;
   }
